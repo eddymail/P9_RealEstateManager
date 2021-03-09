178cc1a08b900de75145d16d37d5bb2b694ec260
Edouard Loussouarn <ed.loussouarn@gmail.com>
2021-03-09 11:00:53 +0100

    AddActivity: Add presentation picture from device Ok, date automatic To do: -Add picture from device take pictures -Add pictures for gallery from device -Map Static for address

--- app/src/main/java/com/openclassrooms/realestatemanager/Activities/AddActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/Activities/AddActivity.java
@@ -1,41 +1,49 @@
 package com.openclassrooms.realestatemanager.Activities;
 
-import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
-import android.support.v4.content.ContextCompat;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.openclassrooms.realestatemanager.Adapter.HouseRecyclerAdapter;
-import com.openclassrooms.realestatemanager.Fragments.MainFragment;
 import com.openclassrooms.realestatemanager.Injection.Injection;
 import com.openclassrooms.realestatemanager.Injection.ViewModelFactory;
 import com.openclassrooms.realestatemanager.Model.House;
 import com.openclassrooms.realestatemanager.R;
-import com.openclassrooms.realestatemanager.Repositories.HouseDataRepository;
 import com.openclassrooms.realestatemanager.Ui.HouseViewModel;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 public class AddActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final long HOUSE_ID = 1;
-    private String category, district, pointOfInterest, address, description, agentName, saleDate, dateOfSale;;
+    private String category, district, pointOfInterest, address, description, agentName, saleDate, upForSale;;
     private int price, area, numberOfRoom, numberOfBedroom, numberOfBathroom;
 
 
     private EditText categoryInput, districtInput, priceInput, areaInput, roomInput, bedroomInput, bathroomInput,
-    pointOfInterestValue, descriptionValue, saleDateValue, dateOfSaleValue, agentNameValue, addressValue;
-    private Button addButton;
+    pointOfInterestValue, descriptionValue, agentNameValue, addressValue;
+    private Button addButton, addPictureButton;
 
     private HouseViewModel houseViewModel;
     private House houseToAdd;
+    private String illustration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,13 +61,13 @@ public class AddActivity extends AppCompatActivity implements View.OnClickListen
     descriptionValue = findViewById(R.id.et_add_activity_description);
     addressValue = findViewById(R.id.et_add_activity_address);
     agentNameValue = findViewById(R.id.et_add_activity_agent_name);
-    saleDateValue = findViewById(R.id.et_add_activity_sale_date);
-    dateOfSaleValue = findViewById(R.id.et_add_activity_date_of_sale);
     addButton = findViewById(R.id.bt_add_activity);
+    addPictureButton = findViewById(R.id.bt_add_activity_add_picture);
 
     configureViewModel();
 
     addButton.setOnClickListener(this);
+    addPictureButton.setOnClickListener(this);
     }
 
     private void configureViewModel() {
@@ -75,8 +83,8 @@ public class AddActivity extends AppCompatActivity implements View.OnClickListen
         address = addressValue.getText().toString();
         description = descriptionValue.getText().toString();
         agentName = agentNameValue.getText().toString();
-        dateOfSale = dateOfSaleValue.getText().toString();
-        saleDate = saleDateValue.getText().toString();
+        Date date = Calendar.getInstance().getTime();
+        upForSale = new SimpleDateFormat("dd-MM-yyyy").format(date);
         price = Integer.parseInt(priceInput.getText().toString());
         area = Integer.parseInt(areaInput.getText().toString());
         numberOfRoom = Integer.parseInt(roomInput.getText().toString());
@@ -84,17 +92,75 @@ public class AddActivity extends AppCompatActivity implements View.OnClickListen
         numberOfBathroom = Integer.parseInt(bathroomInput.getText().toString());
     }
 
-    private void createHouseAndAddItToDatabase() {
-
-        houseToAdd = new House( category, district, price, area,numberOfRoom, numberOfBathroom, numberOfBedroom, pointOfInterest, description, ContextCompat.getDrawable(this,R.drawable.ic_baseline_home_24).toString(), address, true, saleDate, null, agentName);
-        this.houseViewModel.createHouse(houseToAdd);
-    }
 
     @Override
     public void onClick(View view) {
-        collectInput();
-        createHouseAndAddItToDatabase();
-        Toast.makeText(this, "Le bien a été ajouté ", Toast.LENGTH_LONG).show();
-        AddActivity.this.finish();
+
+        switch (view.getId()){
+            case  R.id.bt_add_activity :
+                //Create new house in database
+                collectInput();
+                createHouseAndAddItToDatabase();
+                Toast.makeText(this, "Le bien a été ajouté ", Toast.LENGTH_LONG).show();
+                AddActivity.this.finish();
+                break;
+
+            case R.id.bt_add_activity_add_picture :
+                //Add picture from device
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, 123);
+                break;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123 && resultCode == RESULT_OK) {
+            //Access to picture from data
+            Uri selectedPicture = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            //Cursor for access
+            Cursor cursor = this.getContentResolver().query(selectedPicture, filePathColumn, null, null, null);
+            //position on line (normalement une seule)
+            cursor.moveToFirst();
+            //get path
+         /*   int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String imgPath = cursor.getString(columnIndex);
+            cursor.close();*/
+            //get picture
+          //  Bitmap bmp = BitmapFactory.decodeFile((imgPath));
+            Bitmap bmp = null;
+            try {
+                bmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedPicture);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("Test", "descriptionPicture : no" );
+            }
+            //set picture
+            String descriptionPicture;
+            if ( bmp != null) {
+                descriptionPicture = getStringImage(bmp);
+                Log.e("Test", "descriptionPicture : " + descriptionPicture);
+                this.illustration = descriptionPicture;
+            }
+        }
+    }
+
+    private void createHouseAndAddItToDatabase() {
+        if (illustration == null) {
+            illustration = "https://www.ouestfrance-immo.com/photo-vente-maison-la-roche-sur-yon-85/207/maison-a-vendre-la-roche-sur-yon-15080201_1_1603144038_f056394ffbe2dcc301aac986e8da59a7_crop_295-222_.jpg";
+        }
+        houseToAdd = new House( category, district, price, area,numberOfRoom, numberOfBathroom, numberOfBedroom, pointOfInterest, description, illustration, address, true, upForSale, null, agentName);
+        Log.e("Test", "illustration : " + illustration);
+        this.houseViewModel.createHouse(houseToAdd);
+    }
+
+    public String getStringImage(Bitmap bmp) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+        return "data:image/jpeg;base64," + Base64.encodeToString(imageBytes, Base64.DEFAULT);
     }
 }
